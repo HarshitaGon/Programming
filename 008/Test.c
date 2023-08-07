2f3e434c1d9f5a16bd3338070465bef21955c6d5
HarshitaGon <harshitagon@gmail.com>
2023-08-07 20:27:45 +0530

    Update the Repository

---
+++ 008/Test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    /*int n1, n2, n3, n4, n5;
+    printf("Enter 5 positive integers: ");
+    scanf("%d%d%d%d%d", &n1, &n2, &n3, &n4, &n5);
+
+    int largest = n1;
+    
+    if (n2 > largest)
+        largest = n2;
+    
+    if (n3 > largest)
+        largest = n3;
+
+    if (n4 > largest)
+        largest = n4;
+
+    if (n5 > largest)
+        largest = n5;
+
+    printf("%d is the largest\n", largest);
+
+    return 0;
+}*/
+    
+    int n, laregst = 0;
+    printf()
--- 008/Test.java
+++ 008/Test.java
@@ -1,15 +1,31 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter 5 positive integers: ");
-        int n1 = sc.nextInt();
+        /*int n1 = sc.nextInt();
         int n2 = sc.nextInt();
         int n3 = sc.nextInt();
         int n4 = sc.nextInt();
         int n5 = sc.nextInt();
+
+        int largest = n1;
+
+        if (n2 > largest)
+            largest = n2;
+
+        if (n3 > largest)
+            largest = n3;
+
+        if (n4 > largest)
+            largest = n4;
+            
+        if (n5 > largest)
+            largest = n5;
+        
+        System.out.println(largest + " is the largest");
 
 
     }
@@ -25,3 +41,35 @@ Enter 5 positive integers: 3 2 1 2 2
 3 is the largest
 
 */
+
+
+
+    int n = sc.nextInt();
+    int largest = 0;
+    
+    if (n > largest)
+        largest = n;
+    
+    n = sc.nextInt();
+    
+    if (n > largest)
+        largest = n;
+    
+    n = sc.nextInt();
+    
+    if (n > largest)
+        largest = n;
+    
+    n = sc.nextInt();
+    
+    if (n > largest)
+        largest = n;
+    
+    n = sc.nextInt();
+    
+    if (n > largest)
+        largest = n;
+    
+    System.out.println(largest + " is the largest");
+  }
+}

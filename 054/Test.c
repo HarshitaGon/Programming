d0c3deab7a6cfafd70c58915a1361d6d5dd583af
HarshitaGon <harshitagon@gmail.com>
2023-09-15 21:00:12 +0530

    Update the Repository

--- 054/Test.c
+++ 054/Test.c
@@ -2,27 +2,15 @@
 
 int main(void) {
     int rows;
-    printf("Enter the number of rows (>= 3): ");
+    printf("Enter the number of rows: ");
     scanf("%d", &rows);
 
-    int columns;
-    printf("Enter the numbers of columns (>= 3): ");
-    scanf("%d", &columns);
-
     int i = 1;
-
     while (i <= rows) {
         int j = 1;
 
-        while (j <= rows - i) {
-            printf("  ");
-            ++j;
-        }
-
-        j = 1;
-        
-        while (j <= columns) {
-            if ((i == 1) || (i == rows) || (j == 1) || (j == columns))
+        while (j <= rows) {
+            if (i + j > rows)
                 printf("* ");
             else
                 printf("  ");
--- 054/Test.java
+++ 054/Test.java
@@ -5,23 +5,28 @@ class Test {
         Scanner sc = new Scanner(System.in);
 
         int rows;
-        System.out.print("Enter the number of rows (>= 3): ");
+        System.out.print("Enter the number of rows: ");
         rows = sc.nextInt();
 
-        int columns;
-        System.out.print("Enter the number of columns (>= 3): ");
-        columns = sc.nextInt();
+        // for (int i = 1; i <= rows; ++i) {
+        //     for (int j = 1; j <= rows - i; ++j) {
+        //         System.out.print("  ");
+        //     }
+
+        //     for (int j = 1; j <= i; ++j) {
+        //         System.out.print("* ");
+        //     }
+
+        //     System.out.println();
+        // }
 
         for (int i = 1; i <= rows; ++i) {
-            for (int j = 1; j <= rows - i; ++j) {
-                System.out.print("  ");
-            }
-
-            for (int j = 1; j <= columns; ++j) {
-                if ((i == 1) || (i == rows) || (j == 1) || (j == columns))
+            for (int j = 1; j <= rows; ++j) {
+                if (i + j > rows)
                     System.out.print("* ");
                 else
                     System.out.print("  ");
+
             }
 
             System.out.println();
@@ -30,13 +35,22 @@ class Test {
 }
 
 
+/*
+    Enter the number of rows: 5
+
+            *
+          * *
+        * * *
+      * * * *
+    * * * * *
+*/
 
 /*
-    Enter the number of rows (>= 3): 4
-    Enter the number of columns (>= 3): 5
 
-          * * * * *
-        *       *
-      *       *
-    * * * * *
+11 12 13 14 15
+21 22 23 24 25
+31 32 33 34 35
+41 42 43 44 45
+51 52 53 54 55
+
 */

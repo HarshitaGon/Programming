9a29dad36467a0ac3870fb15970a2fa0772ae53f
HarshitaGon <harshitagon@gmail.com>
2023-11-25 19:48:02 +0530

    Update the Repository

--- 111/Test.c
+++ 111/Test.c
@@ -2,41 +2,93 @@
 #include <string.h>
 #include <stdbool.h>
 
+// Here, const is used to remove the warning.
+// const will be taught later.
 int my_strlen(const char*);
-void my_gets(char *);
+void my_gets(char*);
+void my_puts(char*);
+void my_strcpy(char*, char*);
+void my_strcat(char*, char*);
+int my_strcmp(char*, char*);
 
 int main(void)
 {
-    printf("sizeof \"hello\" - %zu\n", sizeof "hello");
-    // printf("strlen(\"hello\") - %zu\n", strlen("hello"));
-    printf("my_strlen(\"hello\") - %d\n\n", my_strlen("hello"));
+    // printf("sizeof \"hello\" - %zu\n", sizeof "hello");
+    // printf("my_strlen(\"hello\") - %d\n", my_strlen("hello"));
 
-    printf("char arr[] = \"hello world\";\n");
-    char arr[] = "hello world";
+    // printf("\nchar arr[] = \"hello world\";\n");
+    // char arr[] = "hello world";
 
-    printf("sizeof arr - %zu\n", sizeof arr);
-    printf("my_strlen(arr) - %d\n\n", my_strlen(arr));
+    // printf("\nsizeof arr - %zu\n", sizeof arr);
+    // printf("my_strlen(arr) - %d\n", my_strlen(arr));
 
-    printf("arr[5] = '\\0';\n");
-    arr[5] = '\0';
+    // printf("\narr[5] = '\\0';\n");
+    // arr[5] = '\0';
 
-    printf("sizeof arr - %zu\n", sizeof arr);
-    printf("my_strlen(arr) - %d\n\n", my_strlen(arr));
+    // printf("\nsizeof arr - %zu\n", sizeof arr);
+    // printf("my_strlen(arr) - %d\n", my_strlen(arr));
 
-    printf("char arr2[1000];\n");
+    printf("\nchar arr2[1000];\n");
     char arr2[1000];
 
-    printf("gets(arr2);\n");
-    gets(arr2);
+    // printf("\nmy_gets(arr2);\n");
+    // my_gets(arr2);
 
-    printf("sizeof arr2 - %zu\n", sizeof arr2);
-    printf("my_strlen(arr2) - %d\n\n", my_strlen(arr2));
+    // printf("\nsizeof arr2 - %zu\n", sizeof arr2);
+    // printf("my_strlen(arr2) - %d\n", my_strlen(arr2));
 
-    printf("my_gets(arr2);\n");
+    // printf("\nmy_gets(arr2);\n");
+    // my_gets(arr2);
+
+    // printf("\nsizeof arr2 - %zu\n", sizeof arr2);
+    // printf("my_strlen(arr2) - %d\n", my_strlen(arr2));
+
+    // printf("\nmy_puts(arr2); putchar('\\n');\n");
+    // my_puts(arr2);
+    // putchar('\n');
+
+    printf("\nchar arr3[1000];\n");
+    char arr3[1000];
+
+    // printf("\nmy_strcpy(arr3, arr2);\n");
+    // my_strcpy(arr3, arr2);
+
+    // printf("\nmy_puts(arr3); putchar('\\n');\n");
+    // my_puts(arr3);
+    // putchar('\n');
+
+    // printf("\nmy_gets(arr3);\n");
+    // my_gets(arr3);
+
+    // printf("\nmy_strcat(arr3, arr2);\n");
+    // my_strcat(arr3, arr2);
+
+    // printf("\nmy_puts(arr3); putchar('\\n');\n");
+    // my_puts(arr3);
+    // putchar('\n');
+
+    printf("\nmy_gets(arr2);\n");
     my_gets(arr2);
 
-    printf("sizeof arr2 - %zu\n", sizeof arr2);
-    printf("my_strlen(arr2) - %d\n", my_strlen(arr2));
+    printf("\nmy_gets(arr3);\n");
+    my_gets(arr3);
+
+    int v = my_strcmp(arr2, arr3);
+
+    if (v > 0)
+    {
+        printf("\n%s is lexicographically greater than %s\n", arr2, arr3);
+    }
+
+    else if (v == 0)
+    {
+        printf("\n%s is lexicographically equal to %s\n", arr2, arr3);
+    }
+
+    else
+    {
+        printf("\n%s is lexicographically smaller than %s\n", arr2, arr3);
+    }
 
     return 0;
 }
@@ -53,9 +105,119 @@ int my_strlen(const char* str)
     return i;
 }
 
+// void my_gets(char* str)
+// {
+//     int i = 0;
+
+//     while (true)
+//     {
+//         char c;
+//         scanf("%c", &c);
+
+//         if (c == '\n')
+//             break;
+
+//         str[i] = c;
+//         ++i;
+//     }
+
+//     str[i] = '\0';
+// }
+
 void my_gets(char* str)
 {
-    
+    int i = 0;
+
+    while (true)
+    {
+        // We use int c; instead of char c; because the value of EOF is -1, and
+        // if getchar() returns -1, then storing -1 in a char variable will not
+        // be possible on many computers.
+        int c = getchar();
+
+        if ((c == '\n') || (c == EOF))
+            break;
+
+        str[i] = (char) c;
+        ++i;
+    }
+
+    str[i] = '\0';
+}
+
+// void my_puts(char* str)
+// {
+//     for (int i = 0; str[i] != '\0'; ++i)
+//     {
+//         printf("%c", str[i]);
+//     }
+// }
+
+void my_puts(char* str)
+{
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        putchar(str[i]);
+    }
+}
+
+void my_strcpy(char* dst, char* src)
+{
+    int i = 0;
+
+    while (src[i] != '\0')
+    {
+        dst[i] = src[i];
+        ++i;
+    }
+
+    dst[i] = '\0';
+}
+
+void my_strcat(char* dst, char* src)
+{
+    int i = 0;
+
+    while (dst[i] != '\0')
+    {
+        ++i;
+    }
+
+    int j = 0;
+
+    while (src[j] != '\0')
+    {
+        dst[i] = src[j];
+        ++i;
+        ++j;
+    }
+
+    dst[i] = '\0';
+}
+
+int my_strcmp(char* s1, char* s2)
+{
+    int i = 0;
+
+    while ((s1[i] != '\0') && (s2[i] != '\0') && (s1[i] == s2[i]))
+    {
+        ++i;
+    }
+
+    if (s1[i] > s2[i])
+    {
+        return 1;
+    }
+
+    else if (s1[i] == s2[i])
+    {
+        return 0;
+    }
+
+    else
+    {
+        return -1;
+    }
 }
 
 /*

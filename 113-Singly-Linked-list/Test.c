ff94a7192e4a70cf6754b61b726764d3bae5d4ce
HarshitaGon <harshitagon@gmail.com>
2023-12-19 19:47:51 +0530

    Update the Repository

---
+++ 113-Singly-Linked-list/Test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int value;
+	struct node* ptr_next_node;
+}
+
+typedef struct node Node;
+
+int main(void)
+{
+	Node* ptr_first_node
+
+	return 0;
+}
+/*  */

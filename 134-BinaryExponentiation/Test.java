256bf2a23e569a82082b67c6f00f817151fe237d
HarshitaGon <harshitagon@gmail.com>
2025-12-26 18:49:10 +0530

    Update the Repository

--- 134-BinaryExponentiation/Test.java
+++ 134-BinaryExponentiation/Test.java
@@ -8,12 +8,12 @@ class Test {
         base %= MOD;
 
         while (exp > 0) {
-            if ((exp & 1) == 1) {
+            if ((exp & 1) == 1) {   // if exp is odd
                 ans = (ans * base) % MOD;
             }
 
-            base = (base * base) % MOD;
-            exp >>= 1;
+            base = (base * base) % MOD;     // square the base
+            exp >>= 1;    //right shift of the exponent : 13(1101) -> 1101 -> 110 -> 11 -> 1
         }
 
         return ans;
